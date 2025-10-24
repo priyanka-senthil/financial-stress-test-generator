f4c00a3fd6659801ed97cf3aa2205f01f1970e32
priyanka-senthil <priyankasenthil25@gmail.com>
2025-10-24 11:27:20 -0400

    latest data

--- data_pipeline/dags/test_dag.py
+++ data_pipeline/dags/test_dag.py
@@ -1,5 +1,6 @@
 from airflow import DAG
-from airflow.providers.standard.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator
+
 from datetime import datetime
 
 def hello():
